aa4b66e1915c3ef2dc7a12643608553650408ae5
ensomnatt <ensomnatt@protonmail.com>
2025-04-20 15:49:57 +0300

    add install script and systemd file

---
+++ install.sh
@@ -0,0 +1,21 @@
+!#/bin/bash
+
+APP_DIRECTORY=/opt/webfetch
+SYSTEMD_DIRECTORY=/etc/systemd/system
+
+echo "creating app directory"
+sudo mkdir -p $APP_DIRECTORY
+
+echo "building"
+go build -o webfetch cmd/main.go
+
+echo "copying binary file to the app directory"
+sudo cp -r webfetch $APP_DIRECTORY
+
+echo "preparing systemd file"
+sed -i "s/^User=\$/User=$(whoami)/" webfetch.service
+
+echo "copying systemd file to the systemd directory"
+sudo cp -r webfetch.service $SYSTEMD_DIRECTORY
+
+echo "done. run sudo systemctl enable --now webfetch to enable and start the programm"
